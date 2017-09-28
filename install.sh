1433834a8f5a8b7a2f8d74fc55b76add06269e47
Marek Doležel <mdolezel@users.noreply.github.com>
2017-09-28 02:45:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo yum install -y libXft-devel libXinerama-devel fontpackages-devel
 sudo yum install -y git gcc gdb vim
 sudo yum groupinstall -y "X Window System"
 sudo yum install open-vm-tools gnome-terminal
-sudo yum install gdm
+sudo yum install lightdm
 
 
 if [ -d dwm ]
@@ -17,3 +17,5 @@ cd dwm
 
 make
 sudo make install
+sudo systemctl enable lightdm
+sudo systemctl set-default lightdm.target
