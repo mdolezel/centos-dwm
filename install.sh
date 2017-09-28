d7e386a1122572f70a9120515dbf54c77538fc47
Marek Doležel <mdolezel@users.noreply.github.com>
2017-09-28 02:28:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo yum install -y libXft-devel libXinerama-devel fontpackages-devel
 
 sudo yum install -y git gcc gdb
+sudo yum groupinstall -y "X Window System"
+
 
 git clone https://git.suckless.org/dwm
 
