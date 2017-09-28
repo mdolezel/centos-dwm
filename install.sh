73664bc8e641e1f81cb1ae102e674fb9f3daee72
Marek Doležel <mdolezel@users.noreply.github.com>
2017-09-28 02:38:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ sudo yum install -y libXft-devel libXinerama-devel fontpackages-devel
 sudo yum install -y git gcc gdb vim
 sudo yum groupinstall -y "X Window System"
 sudo yum install open-vm-tools gnome-terminal
+sudo yum install gdm
 
 
 if [ -d dwm ]
