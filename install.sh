bfdc2f40c4f2ce21fe047d513d7be085a3af16af
Marek Doležel <mdolezel@users.noreply.github.com>
2017-09-28 02:33:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 sudo yum install -y libXft-devel libXinerama-devel fontpackages-devel
-
-sudo yum install -y git gcc gdb
+sudo yum install -y git gcc gdb vim
 sudo yum groupinstall -y "X Window System"
+sudo yum install open-vm-tools gnome-terminal
 
+
+if [ -d dwm ]
+then
+   rm -rf dwm
+fi
 
 git clone https://git.suckless.org/dwm
+
 
 cd dwm
 
 make
+sudo make install
